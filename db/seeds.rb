9c4d9e2197a2f86f3afc59c175fa5e14148b17e5
Erick Valente <erickhwk@gmail.com>
2022-05-25 15:17:41 -0300

    Set controller and views for Restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "Fuego Celeste Parrillada", address: "Rua Joaquim Távora 1068, São Paulo, Estado de São Paulo 04015-012 Brasil", phone_number: "+55 11 4563-8999", category: "french")
+Restaurant.create(name: "Adega Santiago Shopping Cidade Jardim", address: "Avenida Magalhaes de Castro 12000 Shopping Cidade Jardim, 4º piso, São Paulo, Estado de São Paulo 05676-120 Brasil", phone_number: "+55 11 3758-4446", category: "chinese")
+Restaurant.create(name: "Picanharia dos Amigos - Ipiranga", address: "Rua Pedrália, 370, São Paulo, Estado de São Paulo 04130-080 Brasil", phone_number: "+55 11 5062-4242", category: "italian")
+Restaurant.create(name: "CipóRestô", address: "Rua Olimpiadas 205 Vila Olímpia, São Paulo, Estado de São Paulo 04551-000 Brasil", phone_number: "+55 11 2161-4900", category: "belgian")
+Restaurant.create(name: "Coco Bambu Conceito Eldorado", address: "Avenida Reboucas 3970 Loja 2017, SubSolo 2 - Shopping Eldorado, São Paulo, Estado de São Paulo 05402-600 Brasil", phone_number: "+55 11 3813-2090", category: "japanese")
